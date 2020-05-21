4365ee7776db017268ed23d3cfc87407cbf4cc2b
Lorenzo Simpson <lorenzosimp@gmail.com>
2020-05-21 17:00:14 -0400

    tree mode and iterative count and say

---
+++ string/countAndSay.js
@@ -0,0 +1,20 @@
+var countAndSay = function(n) {   
+    let str = '1';
+
+    for (let i = 2; i<= n; i++) {
+        let tempStr = '';
+        let count = 0;
+
+        for (let j=0; j<str.length; j++) {
+            const char = str.charAt(j)
+        
+            count += 1
+
+            if (char !== str.charAt(j + 1)) {
+                tempStr += count + char;
+                count = 0;
+            }
+        } str = tempStr
+    }    
+    return str
+};
